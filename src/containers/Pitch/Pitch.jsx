5951230b85808289040c4d6cb0ebd621a3fd0095
Paul Glazzard <Paul.Glazzard@Nology.local>
2020-03-24 11:48:39 +0000

    added state to pitch

--- src/containers/Pitch/Pitch.jsx
+++ src/containers/Pitch/Pitch.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Pitch.module.scss";
 
 const Pitch = () => {
+  const [currentTeam, addPlayerToTeam] = useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15
+  ]);
   return (
     <section className={styles.Pitch}>
-      <div className={styles.FrontRow}>
+      {/* <div className={styles.FrontRow}>
         <p>1</p>
         <p>2</p>
         <p>3</p>
@@ -30,7 +47,7 @@ const Pitch = () => {
         <p>11</p>
         <p>14</p>
       </div>
-      <p className={styles.FullBack}>15</p>
+      <p className={styles.FullBack}>15</p> */}
     </section>
   );
 };
--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40591/harry-thacker.jpg?w=540&h=540&mode=crop",
     position: "Hooker",
+    positionNum: 2,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -15,6 +16,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40601/john-afoa.jpg?w=540&h=540&mode=crop",
     position: "Prop",
+    positionNum: 3,
     attacking: "50",
     defending: "50",
     strength: "5",
@@ -26,6 +28,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40600/joe-joyce.jpg?w=540&h=540&mode=crop",
     position: "Lock",
+    positionNum: 4,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -37,6 +40,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40617/steven-luatua.jpg?w=540&h=540&mode=crop",
     position: "Flanker",
+    positionNum: 6,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -48,6 +52,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40609/nathan-hughes.jpg?w=540&h=540&mode=crop",
     position: "No. 8",
+    positionNum: 8,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -59,6 +64,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/42754/chris-vui.jpg?w=540&h=540&mode=crop",
     position: "Lock",
+    positionNum: 5,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -70,6 +76,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40586/dave-attwood.jpg?w=540&h=540&mode=crop",
     position: "Lock",
+    positionNum: 5,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -81,6 +88,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40622/will-capon.jpg?w=540&h=540&mode=crop",
     position: "Hooker",
+    positionNum: 2,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -92,6 +100,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/41955/max-lahiff.jpg?w=540&h=540&mode=crop",
     position: "Prop",
+    positionNum: 3,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -103,6 +112,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40623/yann-thomas.jpg?w=540&h=540&mode=crop",
     position: "Prop",
+    positionNum: 1,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -114,6 +124,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40606/luke-morahan.jpg?w=540&h=540&mode=crop",
     position: "Wing",
+    positionNum: 14,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -125,6 +136,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40583/charles-piutau.jpg?w=540&h=540&mode=crop",
     position: "Fullback",
+    positionNum: 15,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -136,6 +148,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40613/piers-oconnor.jpg?w=540&h=540&mode=crop",
     position: "Centre",
+    positionNum: 13,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -147,6 +160,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40590/harry-randall.jpg?w=540&h=540&mode=crop",
     position: "Scrum Half",
+    positionNum: 9,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -158,6 +172,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40582/callum-sheedy.jpg?w=540&h=540&mode=crop",
     position: "Fly Half",
+    positionNum: 10,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -169,6 +184,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40580/andy-uren.jpg?w=540&h=540&mode=crop",
     position: "Scrum Half",
+    positionNum: 9,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -180,6 +196,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/46345/thumbnail_henry-purdy.jpg?w=540&h=540&mode=crop",
     position: "Wing",
+    positionNum: 11,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -191,6 +208,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/42757/siale-piutau.jpg?w=540&h=540&mode=crop",
     position: "Centre/Wing",
+    positionNum: 13,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -202,6 +220,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40592/ian-madigan.jpg?w=540&h=540&mode=crop",
     position: "Fly Half",
+    positionNum: 10,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -213,6 +232,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/42753/alapati-leiua.jpg?w=540&h=540&mode=crop",
     position: "Wing",
+    positionNum: 11,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -224,6 +244,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40618/tiff-eden.jpg?w=540&h=540&mode=crop",
     position: "Fly Half",
+    positionNum: 10,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -235,6 +256,7 @@ const players = [
     playerImg:
       "https://bristolsport.azureedge.net/media/40596/jake-heenan.jpg?w=540&h=540&mode=crop",
     position: "Flanker",
+    positionNum: 7,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -246,6 +268,7 @@ const players = [
     playerImg:
       "https://cdn.rugbypass.com/player-image.php?img=images/players/804/580x580/1771.jpg",
     position: "Prop",
+    positionNum: 3,
     attacking: "70",
     defending: "80",
     strength: "85",
@@ -257,6 +280,7 @@ const players = [
     playerImg:
       "https://cdn.rugbypass.com/wp/wp-content/uploads/2019/11/Semi-Bears.jpg",
     position: "Centre/Wing",
+    positionNum: 13,
     attacking: "70",
     defending: "80",
     strength: "85",
